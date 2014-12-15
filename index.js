67d299ccb317e9b932d132edbf2f59e724eaeffc
ethers <ethereum@outlook.com>
2014-12-15 13:32:09 -0800

    start dbl sha

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const crypto = require('crypto');
 
 
-exports.getProof = function() {
-  sha256 = crypto.createHash('SHA256');
-  return sha256;
+exports.getProof = function(txs) {
+  var dhash = txs.map(function(txStr) {
+    var txBuf = new Buffer(txStr, 'hex'),
+      txHash;
+
+    sha256 = crypto.createHash('SHA256');
+    txHash = sha256.update(txBuf).digest('hex');
+
+    sha256 = crypto.createHash('SHA256');
+    return sha256.update(txHash).digest('hex');
+  })
+
+  console.log('dhash: ', dhash)
+
+  return dhash;
 }
