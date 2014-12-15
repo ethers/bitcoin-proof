d62f4ef7cbd397549cc57ee7e66d4cfe74a334c2
ethers <ethereum@outlook.com>
2014-12-15 14:44:30 -0800

    forming tree; probably need to flip bytes

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ const crypto = require('crypto');
 
 
 exports.getProof = function(txs) {
-  var dhash = txs.map(function(txStr) {
-    var txBuf = new Buffer(txStr, 'hex'),
-      txHash;
+  // if (txs.length == 0) {
+  //   return [util.NULL_HASH.slice(0)];
+  // }
 
-    sha256 = crypto.createHash('SHA256');
-    txHash = sha256.update(txBuf).digest('hex');
-
-    sha256 = crypto.createHash('SHA256');
-    return sha256.update(txHash).digest('hex');
+  // adapted from BitcoinJ and bitcore
+  var tree = txs.map(function(txStr) {
+    var txHex = new Buffer(txStr, 'hex');
+    return twoSha256(txHex);
   })
 
-  console.log('dhash: ', dhash)
+  var j = 0;
+  // Now step through each level ...
+  for (var size = txs.length; size > 1; size = Math.floor((size + 1) / 2)) {
+    // and for each leaf on that level ..
+    for (var i = 0; i < size; i += 2) {
+      var i2 = Math.min(i + 1, size - 1);
+      var a = tree[j + i];
+      var b = tree[j + i2];
+      tree.push(twoSha256(Buffer.concat([a, b])));
+    }
+    j += size;
+  }
 
-  return dhash;
+  return tree[tree.length - 1].toString('hex');
 }
+
+var sha256 = exports.sha256 = function(data) {
+  return new Buffer(crypto.createHash('sha256').update(data).digest('binary'), 'binary');
+};
+
+var twoSha256 = exports.twoSha256 = function(data) {
+  return sha256(sha256(data));
+};
